a11f65f32519c4e985cea4d033a3ca8dfcd8ceec
Layuz <yeinervm06@hotmail.com>
2025-07-11 20:34:49 -0500

    v1.2.4

--- JS/graficas.js
+++ JS/graficas.js
@@ -1,20 +1,71 @@
-  const ctx = document.getElementById('grafica-1');
+/* ========== GRAFICA 1# ========== */
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
+const ctx_1 = document.getElementById('grafica-1');
+const DATA_COUNT_1 = 7;
+const NUMBER_CFG_1 = {count: DATA_COUNT_1, min: -100, max: 100};
+
+const labels_1 = ["a", "c", "d", "x", "g", "e", "f"];
+const data_1 = {
+  labels: labels_1,
+  datasets: [
+    {
+      label: 'Fully Rounded',
+      data: [1,2,3,23,43,12,12],
+      borderColor: "red",
+      backgroundColor: "rgba(255, 20, 78, 0.5)",
+      borderWidth: 2,
+      borderRadius: Number.MAX_VALUE,
+      borderSkipped: false,
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+    {
+      label: 'Small Radius',
+      data: [11,-20,34,30,3,2,15],
+      borderColor: "blue",
+      backgroundColor: "blue",
+      borderWidth: 2,
+      borderRadius: 5,
+      borderSkipped: false,
+    }
+  ]
+};
+
+const config_1 = {
+  type: 'bar',
+  data: data_1,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart'
       }
     }
-  });
+  },
+};
+
+new Chart(ctx_1, config_1)
+
+/* ========== GRAFICA 2# ========== */
+
+/* ========== GRAFICA 3# ========== */
+
+/* ========== GRAFICA 4# ========== */
+
+/* ========== GRAFICA 5# ========== */
+
+/* ========== GRAFICA 6# ========== */
+
+/* ========== GRAFICA 7# ========== */
+
+/* ========== GRAFICA 8# ========== */
+
+/* ========== GRAFICA 9# ========== */
+
+/* ========== GRAFICA 10# ========== */
+
+/* ========== GRAFICA 11# ========== */
+
+/* ========== GRAFICA 12# ========== */
